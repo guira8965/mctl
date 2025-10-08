e94fb381aa11c8388b1a77510a3da0f204c1f7b3
guira <39302280+guira8965@users.noreply.github.com>
2025-10-08 09:11:28 +0000

    Basic functionality on start() and stop(). Also added brightness control.

--- mctl.sh
+++ mctl.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-MOONLIGHT="/usr/bin/moonlight"
-SCREEN="/usr/bin/screen"
+# ------------------------------------------------------------
+# PACKAGES
+# ------------------------------------------------------------
+MOONLIGHT_PATH="/usr/bin/moonlight"
+SCREEN_PATH="/usr/bin/screen"
+
+HOST="192.168.2.2"
+APP="gui"
 
 # ------------------------------------------------------------
 # CONFIGURATION
 # ------------------------------------------------------------
 # MOONLIGHT CONFIG
-moonlight (){
-    local IP_ADDRESS="192.168.2.2"
-    local RESOLUTION="1366x768"
-    local BITRATE="600000" # KBps
-    echo $MOONLIGHT $IP_ADDRESS $RESOLUTION $BITRATE 
+moonlight(){
+    local RESOLUTION="--resolution 1366x768"
+    local FPS="--fps 60"
+    local BITRATE="--bitrate 200000" # KBps
+    local PACKET_SIZE="--packet-size 9000" # MTU
+    local DISPLAY_MODE="--display-mode borderless" # borderless/fullscreen/windowed
+    local VIDEO_ENCORDER="--video-decoder hardware" # auto/hardware/software
+    local VIDEO_CODEC="--video-codec auto" # AV1/H.264/HEVC/auto
+    local PERFORMANCE_OVERLAY="--performance-overlay" # --performance-overlay/--no-performance-overlay 
+    local OTHER_OPTIONS=""
+    local PARAMETERS="$RESOLUTION $FPS $BITRATE $PACKET_SIZE $DISPLAY_MODE $VIDEO_ENCORDER $VIDEO_CODEC $OTHER_OPTIONS"
+    echo "$MOONLIGHT_PATH stream $PARAMETERS $HOST $APP" # Usage: moonlight [options] stream <host> "<app>"
 }
-
 # SCREEN CONFIG
-screen () {
+screen() {
     PROCESS_NAME="moonlight"
-    ARGUMENTS="-dmS"
-    echo $ARGUMENTS $PROCESS_NAME  
+    ARGUMENTS="-DmS"
+    echo "$SCREEN_PATH $ARGUMENTS $PROCESS_NAME"
 }
 
 # ------------------------------------------------------------
@@ -26,8 +38,18 @@ screen () {
 # ------------------------------------------------------------
 
 start(){
-    local run=$(moonlight) $(screen)
-    run
+    local brightness="sudo brightnessctl set 0%"
+    local run="$(screen) $(moonlight)"
+    $brightness
+    $run
 }
-
+stop(){
+    local brightness="sudo brightnessctl set 0%"
+    local disconnect="moonlight quit $HOST"
+    local kill="killall -9 moonlight"
+    $brightness
+    $disconnect
+    $kill
+}
 start
+stop
