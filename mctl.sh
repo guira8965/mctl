361eb682150dfc64807dcd495d4f6451c1e493b5
guira <39302280+guira8965@users.noreply.github.com>
2025-10-08 04:15:01 +0000

    psuedocode

---
+++ mctl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MOONLIGHT="/usr/bin/moonlight"
+SCREEN="/usr/bin/screen"
+
+# MOONLIGHT CONFIGURATION
+IP_ADDRESS="192.168.2.2"
+RESOLUTION="1366x768"
+BIRATE="600000" # KBps
+
+start(){
+    
+}
+
+stop(){
+
+}
