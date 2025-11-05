b7e80e7828b3b548534118efd7c3e660b96dfdd6
guira8965 <guira8965@gmail.com>
2025-11-05 20:08:28 +0800

    FEAT: Implemented the ability to configurate config files within the app and config.yaml.

--- mctl.py
+++ mctl.py
@@ -1,17 +1,29 @@
-import yaml, sys, subprocess
+import sys, subprocess
+from ruamel.yaml import YAML
 # author: guira8965 (github)
 
 # ------------------------------------------------------------
-# Load Config
+# Load Config & Global Variables
 # ------------------------------------------------------------
 CONFIG_PATH = "config.yaml"
-def load_config():
-    with open(CONFIG_PATH, 'r') as file:
-        return yaml.safe_load(file)
-try: 
-    config = load_config()
-except FileNotFoundError:
-    sys.exit("Error: config.yaml not found.")
+
+yaml = YAML()
+yaml.preserve_quotes = True
+
+def read_config():    
+    try: 
+        with open(CONFIG_PATH, "r", encoding="utf-8") as file:
+            return yaml.load(file)
+    except Exception as e:
+        sys.exit(f"Error reading {CONFIG_PATH}: {e}")
+config = read_config()
+
+def write_config(input):
+    try:
+        with open(CONFIG_PATH, "w", encoding="utf-8") as file:
+            return yaml.dump(input, file)
+    except Exception as e:
+        sys.exit(f"Error writing to {CONFIG_PATH}: {e}")
 
 ssh_host            = config["windows"]["ssh_host"]
 ssh_port            = config["windows"]["ssh_port"]
@@ -29,8 +41,26 @@ packet_size         = config["moonlight"]["packet_size"]
 display_mode        = config["moonlight"]["display_mode"]
 video_encoder       = config["moonlight"]["video_encoder"]
 video_codec         = config["moonlight"]["video_codec"]
-performance_overlay = config["moonlight"]["performance_overlay"]
-other_options       = config["moonlight"]["other_options"]
+
+if config["moonlight"]["performance_overlay"]:
+    performance_overlay = "--performance-overlay"
+else:
+    performance_overlay = "--no-performance-overlay"
+
+if config["moonlight"]["vsync"]:
+    vsync = "--vsync"
+else:
+    vsync = "--no-vsync"
+
+if config["moonlight"]["frame_pacing"]:
+    frame_pacing = "--frame-pacing"
+else:
+    frame_pacing = "--no-frame-pacing"
+
+if config["moonlight"]["game_optimization"]:
+    game_optimization = "--game-optimization"
+else:
+    game_optimization = "--no-game-optimization"
 
 # ------------------------------------------------------------
 # Helper functions
@@ -45,7 +75,29 @@ def screen(name, cmd):
     ssh(f"{SCREEN_PATH} -dmS {name} bash -c {cmd}")
 def moonlight(name, cmd):
     screen(name, f"'export DISPLAY=:{DISPLAY_NUMBER}; {MOONLIGHT_PATH} {cmd}'")
-
+def modify(type, entity, attribute, prompt):
+    if type == "default":
+        config[f'{entity}'][f'{attribute}'] = input(f"{prompt} ").strip()
+        write_config(config)
+    elif type == "array":
+        while True:
+            try:
+                amount = int(input("Enter number of elements: ").strip())
+                break
+            except ValueError :
+                print("Invalid input. Please enter a number.")
+        new_elements = []
+        for i in range(amount):
+            element = input(f"Enter element #{i+1}: ").strip()
+            new_elements.append(element)
+        config[f'{entity}'][f'{attribute}'] = new_elements
+        write_config(config)
+    elif type == "conditional":
+        if config[f'{entity}'][f'{attribute}'] == True:
+            config[f'{entity}'][f'{attribute}'] = False
+        else:
+            config[f'{entity}'][f'{attribute}'] = True
+        write_config(config)
 # ------------------------------------------------------------
 # Main Functions
 # ------------------------------------------------------------
@@ -56,10 +108,10 @@ def start_moonlight():
         "start_moonlight",
         f"stream --resolution {resolution} --fps {fps} --bitrate {bitrate} --packet-size {packet_size} "
         f"--display-mode {display_mode} --video-decoder {video_encoder} --video-codec {video_codec} "
-        f"{other_options} {performance_overlay} {moonlight_host} {moonlight_app}"
+        f"{vsync} {frame_pacing} {game_optimization} {performance_overlay} {moonlight_host} {moonlight_app}"
     )
 def start_usbip():
-    print(f"Starting Webcam on {usbip_host}")
+    print(f"Starting USB/IP on {usbip_host}")
     run("usbip detach -a")
     for x in usbip_bus_id:
         run(f"usbip attach -r {usbip_host} -b {x}")
@@ -74,91 +126,208 @@ def stop_moonlight():
 def stop_usbip():
     print(f"Stopping USB/IP on {usbip_host}")
     run(f"usbip detach -a")
+    ssh(f"systemctl restart usbip-bind@1bcf:2b90.service")
 def start():
     clear()
-    print(f"--- mctl ---")
-    print(f"[A] Start All")
-    print(f"[1] Start Moonlight")
-    print(f"[2] Start USB/IP")
-    print(f"[X] Exit")
-    choice = input(f"Select an option: ").strip().lower()
+    print(f"--- mctl/start ---")
+    print(f"[1] Start All")
+    print(f"[2] Start Moonlight")
+    print(f"[3] Start USB/IP")
+    print(f"[0] Exit")
+    choice = input(f"Select an option: ").strip()
     match choice:
-        case "a":
-            start_usbip()
-            start_moonlight()
-            input(f"Press Enter to return...")
-            start()
         case "1":
+            start_usbip()
             start_moonlight()
             input(f"Press Enter to return...")
             start()
         case "2":
+            start_moonlight()
+            input(f"Press Enter to return...")
+            start()
+        case "3":
             start_usbip()
             input(f"Press Enter to return...")
             start()
-        case "x":
+        case "0":
             menu()
         case _:
             start()
 def stop():
     clear()
-    print(f"--- mctl ---")
-    print (f"[A] Stop All")
-    print (f"[1] Stop Moonlight")
-    print (f"[2] Stop USB/IP")
-    print (f"[X] Exit")
-    choice = input(f"Select an option: ").strip().lower()
+    print(f"--- mctl/stop ---")
+    print(f"[1] Stop All")
+    print(f"[2] Stop Moonlight")
+    print(f"[3] Stop USB/IP")
+    print(f"[0] Exit")
+    choice = input(f"Select an option: ").strip()
     match choice:
-        case "a":
-            stop_usbip()
-            stop_moonlight()
-            input(f"Press Enter to return...")
-            stop()
         case "1":
+            stop_usbip()
             stop_moonlight()
             input(f"Press Enter to return...")
             stop()
         case "2":
+            stop_moonlight()
+            input(f"Press Enter to return...")
+            stop()
+        case "3":
             stop_usbip()
             input(f"Press Enter to return...")
             stop()
-        case "x":
+        case "0":
             menu()
         case _:
             stop()
+def configure():
+    while True:
+        clear()
+        print(f"--- mctl/configure ---")
+        print(f"[1] Windows Settings")
+        print(f"[2] Moonlight Settings")
+        print(f"[3] Linux Settings")
+        print(f"[0] Exit")
+        choice = input(f"Select an option: ").strip()
+        match choice:
+            case "1":
+                while True:
+                    clear()
+                    print(f"--- mctl/configure/windows ---")
+                    print(f"[1] SSH Host ({config['windows']['ssh_host']})")
+                    print(f"[2] SSH Port ({config['windows']['ssh_port']})")
+                    print(f"[3] USB/IP Host ({config['windows']['usbip_host']})")
+                    print(f"[4] USB/IP Bus ID ({config['windows']['usbip_bus_id']})")
+                    print(f"[0] Exit")
+                    choice = input(f"Select an option: ").strip()
+                    match choice:
+                        case "1":
+                            modify("default", "windows", "ssh_host", "Enter SSH Host (e.g. user@192.168.0.1):")
+                        case "2":
+                            modify("default", "windows", "ssh_port", "Enter SSH Port (e.g. 22): ")
+                        case "3":
+                            modify("default", "windows", "usbip_host", "Enter USB/IP Host (e.g. 192.168.0.1):")
+                        case "4":
+                            modify("array", "windows", "usbip_bus_id", "Enter number of USB/IP Bus IDs: (e.g. 2-5):")
+                        case "0":
+                            configure()
+                        case _:
+                            continue
+            case "2":
+                while True:
+                    clear()
+                    print(f"--- mctl/configure/moonlight ---")
+                    print(f"[1] Moonlight Host ({config['moonlight']['moonlight_host']})")
+                    print(f"[2] Moonlight App ({config['moonlight']['moonlight_app']})")
+                    print(f"[3] Resolution ({config['moonlight']['resolution']})")
+                    print(f"[4] FPS ({config['moonlight']['fps']})")
+                    print(f"[5] Bitrate ({config['moonlight']['bitrate']})")
+                    print(f"[6] Packet Size ({config['moonlight']['packet_size']})")
+                    print(f"[7] Display Mode ({config['moonlight']['display_mode']})")
+                    print(f"[8] Video Encoder ({config['moonlight']['video_encoder']})")
+                    print(f"[9] Video Codec ({config['moonlight']['video_codec']})")
+                    print(f"[10] Performance Overlay ({config['moonlight']['performance_overlay']})")
+                    print(f"[11] VSync ({config['moonlight']['vsync']})")
+                    print(f"[12] Frame Pacing ({config['moonlight']['frame_pacing']})")
+                    print(f"[13] Game Optimization ({config['moonlight']['game_optimization']})")
+                    print(f"[0] Exit")
+                    choice = input(f"Select an option: ").strip()
+                    match choice:
+                        case "1":
+                            modify("default", "moonlight", "moonlight_host", "Enter Moonlight Host (e.g. 192.168.0.1):")
+                        case "2":
+                            modify("default", "moonlight", "moonlight_app", "Enter Moonlight App (e.g. 22):")
+                        case "3":
+                            modify("default", "moonlight", "resolution", "Enter Resolution (e.g. 1920x1080):")
+                        case "4":
+                            modify("default", "moonlight", "fps", "Enter FPS (e.g. 60):")
+                        case "5":
+                            modify("default", "moonlight", "bitrate", "Enter Bitrate (e.g. 100000 [in KBps]):")
+                        case "6":
+                            modify("default", "moonlight", "packet_size", "Enter Packet Size (e.g. 1492 [in MTU]):")
+                        case "7":
+                            modify("default", "moonlight", "display_mode", "Enter Display Mode (borderless/fullscreen/windowed):")
+                        case "8":
+                            modify("default", "moonlight", "video_encoder", "Enter Video Encoder (auto/hardware/software):")
+                        case "9":
+                            modify("default", "moonlight", "video_coder", "Enter Video Codec (auto/AV1/H.264/HEVC):")
+                        case "10":
+                            modify("conditional", "moonlight", "performance_overlay", None)
+                        case "11":
+                            modify("conditional", "moonlight", "vsync", None)
+                        case "12":
+                            modify("conditional", "moonlight", "frame_pacing", None)
+                        case "13":
+                            modify("conditional", "moonlight", "game_optimization", None)
+                        case "0":
+                            configure()
+                        case _:
+                            continue
+            case "3":
+                while True:
+                    clear()
+                    print(f"--- mctl/configure/linux ---")
+                    print(f"[1] Moonlight Path ({config['linux']['MOONLIGHT_PATH']})")
+                    print(f"[2] Screen Path ({config['linux']['SCREEN_PATH']})")
+                    print(f"[3] Display Number ({config['linux']['DISPLAY_NUMBER']})")
+                    print(f"[0] Exit")
+                    choice = input(f"Select an option: ").strip()
+                    match choice:
+                        case "1":
+                            modify("default", "linux", "MOONLIGHT_PATH", "Enter Moonlight Path (e.g. /usr/bin/moonlight):")
+                        case "2":
+                            modify("default", "linux", "SCREEN_PATH", "Enter Screen Path (e.g. 22):")
+                        case "3":
+                            modify("default", "linux", "DISPLAY_NUMBER", "Enter Display Number (e.g. 0):")
+                        case "0":
+                            configure()
+                        case _:
+                            continue
+            case "0":
+                menu()
+            case _:
+                continue
 def status():
     ssh(f"screen -ls")
     input(f"Press Enter to return...")
     menu()
 def menu():
-    clear()
-    print(f"--- mctl ---")
-    print(f"[1] Start")
-    print(f"[2] Stop")
-    print(f"[3] Status")
-    print(f"[X] Exit")
-    choice = input(f"Select an option: ").strip().lower()
-    match choice:
-        case "1":
-            start()
-        case "2":
-            stop()
-        case "3":
-            status()
-        case "x":
-            choice = input(f"Are you sure you want to exit? (y/n): ").strip().lower()
-            if choice == "n":
-                menu()
-            else:
-                sys.exit(0)
-        case _:
-            menu()
+    while True:
+        clear()
+        print(f"--- mctl ---")
+        print(f"[1] Start")
+        print(f"[2] Stop")
+        print(f"[3] Configure")
+        print(f"[4] Status")
+        print(f"[0] Exit")
+        choice = input(f"Select an option: ").strip()
+        match choice:
+            case "1":
+                start()
+            case "2":
+                stop()
+            case "3":
+                configure()
+            case "4":
+                status()
+            case "0":
+                while True:
+                    choice = input(f"Are you sure you want to exit? (Y/n): ").strip().lower() or "y"
+                    if choice == "n":
+                        menu()
+                    elif choice == "y":
+                        print("Exiting program...")
+                        sys.exit(0)
+                    else:
+                        print("Invalid input. Please try again.")
+            case _:
+                continue
+
 # ------------------------------------------------------------
 # Entry function
 # ------------------------------------------------------------
 if __name__ == "__main__":
     try:
-        menu()
+        configure()
     except KeyboardInterrupt:
         clear()
         print("Exiting mctl.")
